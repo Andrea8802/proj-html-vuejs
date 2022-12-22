a8002228151b05c2e2d25a68c734854cce7c5616
Andrea8802 <andreabonaventura802@gmail.com>
2022-12-22 16:12:47 +0100

    Creato form input footer

--- src/components/Footer/TopFooter.vue
+++ src/components/Footer/TopFooter.vue
@@ -14,7 +14,7 @@ export default {
             </div>
 
             <div class="title">
-                Get in touch
+                Get in
                 <div class="bg-title">
                     Touch
                 </div>
@@ -23,6 +23,26 @@ export default {
             <div class="text">
                 We will respond to your message as soon as possible
             </div>
+
+            <form>
+                <input type="text" name="name" placeholder="Name" required>
+
+                <input type="email" name="email" placeholder="Email" required>
+
+                <input type="tel" name="phone" placeholder="Phone">
+
+                <select name="type">
+                    <option value="">
+                        More info
+                    </option>
+                </select>
+
+                <input type="text" name="message" placeholder="Message" required minlength="10" class="message">
+
+                <button type="submit" class="button solid">
+                    SEND
+                </button>
+            </form>
         </div>
 
         <div class="right">
@@ -61,6 +81,41 @@ section {
             color: $text-color;
             font-size: 14px;
         }
+
+        form {
+            margin-top: 30px;
+            width: 70%;
+            @include d-flex(flex-start, center);
+            flex-wrap: wrap;
+            gap: 10px;
+
+            input,
+            select {
+                background-color: #1d1d23;
+                color: $light-text-color;
+                border: none;
+                width: calc(50% - 10px);
+                padding: 12px;
+                border-radius: 3px;
+            }
+
+            select {
+                color: $text-color;
+            }
+
+            .message {
+                width: calc(100% - 10px);
+                padding: 10px 10px 100px;
+            }
+
+            button {
+                border: none;
+                margin-top: 15px;
+            }
+        }
+
+
+
     }
 }
 </style>
